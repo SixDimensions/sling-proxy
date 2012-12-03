801a5011daa48c619dbce67fcb83b7d46cf62537
dklco <dklco@sixdimensions.com>
2012-12-03 09:25:44 -0500

    Adding JavaDoc, annotations and cleaning up some of the code

--- src/main/java/org/apache/sling/commons/proxy/ProxyAnnotationServiceManager.java
+++ src/main/java/org/apache/sling/commons/proxy/ProxyAnnotationServiceManager.java
@@ -1,8 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sling.commons.proxy;
 
+/**
+ * The ProxyAnnotationServiceManager manages the available
+ * ProxyAnnotationService instances.
+ * 
+ * @author dklco
+ */
 public interface ProxyAnnotationServiceManager {
+
+	/**
+	 * Get the ProxyAnnotationService instance associated with the specified
+	 * annotation class.
+	 * 
+	 * @param annotation
+	 *            the Java class for the annotation associated with the
+	 *            ProxyAnnotationService
+	 * @return the ProxyAnnotationService associated with the annotation class
+	 *         or null
+	 */
+	public ProxyAnnotationService getProxyAnnotationService(Class<?> annotation);
+
+	/**
+	 * Registers a new ProxyAnnotationService instance associated with the
+	 * specified annotation class. Will replace any existing
+	 * ProxyAnnotationServices for the specified annotation class.
+	 * 
+	 * @param annotationClass
+	 *            the Java class for the annotation to associate with the
+	 *            ProxyAnnotationService
+	 * @param service
+	 *            the ProxyAnnotationService instance
+	 */
 	public void registerProxyAnnotationService(Class<?> annotationClass,
 			ProxyAnnotationService service);
 
-	public ProxyAnnotationService getProxyAnnotationService(Class<?> annotation);
+	/**
+	 * Unregisters a new ProxyAnnotationService instance associated with the
+	 * specified annotation class.
+	 * 
+	 * @param annotationClass
+	 *            the Java class for the annotation associated with the
+	 *            ProxyAnnotationService to remove
+	 */
+	public void unregisterProxyAnnotationService(Class<?> annotationClass);
 }
--- src/main/java/org/apache/sling/commons/proxy/impl/ProxyAdapterFactory.java
+++ src/main/java/org/apache/sling/commons/proxy/impl/ProxyAdapterFactory.java
@@ -1,29 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sling.commons.proxy.impl;
 
 import java.lang.reflect.Proxy;
 
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.adapter.AdapterFactory;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.commons.classloader.DynamicClassLoaderManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Adaptor Factory for adapting Sling Resources to Sling Dynamic Proxies. This
+ * adaptor utilizes Java Dynamic Proxies to allow adapting resources into
+ * objects which proxy calls against interface methods into the resource
+ * properties.
+ * 
+ * @author dklco
+ */
+@Component(label = "Proxy Adapter Factory", name = "org.apache.sling.commons.proxy.impl.ProxyAdapterFactory", metatype = true, immediate = true)
+@Service(value = AdapterFactory.class)
+@Properties(value = {
+		@Property(name = AdapterFactory.ADAPTABLE_CLASSES, value = "org.apache.sling.api.resource.Resource"),
+		@Property(name = AdapterFactory.ADAPTER_CLASSES, value = "org.apache.sling.commons.proxy.SlingProxy"),
+		@Property(name = "service.vendor", value = "The Apache Software Foundation"),
+		@Property(name = "service.description", value = "Apache Sling Proxy Adapter Factory") })
 public class ProxyAdapterFactory implements AdapterFactory {
 
+	/**
+	 * Reference to the Sling Dynamic ClassLoader Manager.
+	 */
 	@Reference
 	private DynamicClassLoaderManager classLoaderManager;
 
+	/**
+	 * The SLF4J Logger.
+	 */
+	private static final Logger log = LoggerFactory
+			.getLogger(ProxyAdapterFactory.class);
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.apache.sling.api.adapter.AdapterFactory#getAdapter(java.lang.Object,
+	 * java.lang.Class)
+	 */
 	public <AdapterType> AdapterType getAdapter(Object adaptable,
 			Class<AdapterType> type) {
+		log.trace("getAdapter");
 
 		if (adaptable instanceof Resource) {
-
 			ClassLoader classLoader = classLoaderManager
 					.getDynamicClassLoader();
 
 			Resource resource = (Resource) adaptable;
-			return (AdapterType) Proxy.newProxyInstance(classLoader,
-					new Class[] { type.getClass() }, new SlingDynamicProxy(
-							resource));
+
+			log.warn("Creating Dynamic Proxy of type: {}", type.getName());
+			return type.cast(Proxy.newProxyInstance(classLoader,
+					new Class[] { type }, new SlingDynamicProxy(resource)));
+		} else {
+			log.warn("Unable to adapt object of type: {}", adaptable.getClass()
+					.getName());
 		}
 		return null;
 
