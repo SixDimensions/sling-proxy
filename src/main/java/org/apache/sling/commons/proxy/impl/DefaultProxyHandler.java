42a5f61acb291e3f2c246082c5459ba2427727fc
dklco <dklco@sixdimensions.com>
2012-12-03 10:48:07 -0500

    Cleaned up and completed the SlingDynamicProxy and DefaultProxyHandler. In theory, the basic functionality should now be complete... in theory :-)

--- src/main/java/org/apache/sling/commons/proxy/impl/DefaultProxyHandler.java
+++ src/main/java/org/apache/sling/commons/proxy/impl/DefaultProxyHandler.java
@@ -1,12 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sling.commons.proxy.impl;
 
 import java.lang.reflect.Method;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.commons.proxy.ProxyAnnotationService;
 import org.apache.sling.commons.proxy.SlingProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * The Default Proxy Handler, this class will attempt to retrieve a property
+ * from the resource with a key matching the method name according to the
+ * findMatchingKey method.
+ * 
+ * @author dklco
+ */
 public class DefaultProxyHandler implements ProxyAnnotationService {
+
+	private static final String GET_RESOURCE_METHOD_NAME = "getBackingResource";
+	private static final Logger log = LoggerFactory
+			.getLogger(DefaultProxyHandler.class);
 
 	/*
 	 * (non-Javadoc)
@@ -18,7 +50,55 @@ public class DefaultProxyHandler implements ProxyAnnotationService {
 	 */
 	public Object invoke(Resource resource, SlingProxy proxy, Method m,
 			Object[] args) throws Throwable {
-		// TODO Auto-generated method stub
+		log.trace("invoke");
+
+		String methodName = m.getName();
+		log.debug("Handling method: {}" + methodName);
+
+		ValueMap properties = resource.adaptTo(ValueMap.class);
+
+		if (GET_RESOURCE_METHOD_NAME.equals(methodName)) {
+			return resource;
+		} else if (methodName.startsWith("get")) {
+			String name = methodName.substring(methodName.indexOf("get") + 3);
+			return properties.get(findMatchingKey(properties, name),
+					m.getReturnType());
+		} else if (methodName.startsWith("is")) {
+			String name = methodName.substring(methodName.indexOf("is") + 2);
+			return properties.get(findMatchingKey(properties, name),
+					m.getReturnType());
+		}
 		return null;
+	}
+
+	/**
+	 * Finds the key matching the specified name based on the following rules:
+	 * <ul>
+	 * <li>First checks to see if a property with the name exists</li>
+	 * <li>Attempt to find a rule with a similar name by removing any colons
+	 * from the keys and doing a case insensitive comparison
+	 * <li>
+	 * <li>If no key is found, will return the provided name</li>
+	 * </ul>
+	 * 
+	 * @param properties
+	 *            the current ValueMap of properties to check
+	 * @param name
+	 *            the name of the property to attempt to retrieve
+	 * @return
+	 */
+	protected String findMatchingKey(ValueMap properties, String name) {
+		log.trace("findMatchingKey");
+		if (properties.containsKey(name)) {
+			return name;
+		} else {
+			for (String key : properties.keySet()) {
+				if (key.replace(":", "").equalsIgnoreCase(name)) {
+					log.debug("Found key {} matching name {}", key, name);
+					return key;
+				}
+			}
+		}
+		return name;
 	}
 }
--- src/main/java/org/apache/sling/commons/proxy/impl/SlingDynamicProxy.java
+++ src/main/java/org/apache/sling/commons/proxy/impl/SlingDynamicProxy.java
@@ -1,16 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sling.commons.proxy.impl;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.commons.proxy.ProxyAnnotationService;
 import org.apache.sling.commons.proxy.ProxyAnnotationServiceManager;
+import org.apache.sling.commons.proxy.SlingProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Handles the invocation of methods on SlingProxy interfaces. Retrieves the
+ * annotations for the method being invoked and attempts to find a
+ * ProxyAnnotationService for the annotation and invoke that, if no suitable
+ * ProxyAnnotationService is found, it will invoke the DefaultProxyHandler.
+ * 
+ * @author dklco
+ */
 public class SlingDynamicProxy implements InvocationHandler {
 
+	/**
+	 * The resource which has been adapted for this Dynamic Proxy.
+	 */
 	private final Resource resource;
-	private final ProxyAnnotationServiceManager proxyAnnotationServiceManager;
 
+	/**
+	 * The ProxyAnnotationServiceManager, used to hold references to the Proxy
+	 * Annotation Services.
+	 */
+	private final ProxyAnnotationServiceManager proxyAnnotationServiceManager;
+	private static final Logger log = LoggerFactory
+			.getLogger(SlingDynamicProxy.class);
+
+	/**
+	 * Construct a new Sling Dynamic Proxy instance.
+	 * 
+	 * @param resource
+	 *            this resource will be used to back the proxy
+	 * @param proxyAnnotationServiceManager
+	 *            the proxy annotations service manager reference
+	 */
 	public SlingDynamicProxy(final Resource resource,
 			final ProxyAnnotationServiceManager proxyAnnotationServiceManager) {
 		this.resource = resource;
@@ -25,7 +74,23 @@ public class SlingDynamicProxy implements InvocationHandler {
 	 */
 	public Object invoke(Object proxy, Method m, Object[] args)
 			throws Throwable {
-		// TODO: Handle the invocation target here
-		return null;
+		log.trace("invoke");
+
+		ProxyAnnotationService proxyAnnotationService = new DefaultProxyHandler();
+		for (Annotation annotation : m.getAnnotations()) {
+			if (proxyAnnotationServiceManager
+					.getProxyAnnotationService(annotation.getClass()) != null) {
+				proxyAnnotationService = proxyAnnotationServiceManager
+						.getProxyAnnotationService(annotation.getClass());
+				log.debug(
+						"Found Proxy Annotation Service {} for annotation {}",
+						proxyAnnotationService.getClass().getName(), annotation
+								.getClass().getName());
+				break;
+			}
+		}
+
+		return proxyAnnotationService.invoke(resource,
+				SlingProxy.class.cast(proxy), m, args);
 	}
 }
